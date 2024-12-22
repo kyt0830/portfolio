f86b60e4204ecafa37c50a703051dd2ac627f972
kyt0830 <kyt910830@gmail.com>
2024-12-22 09:34:40 +0900

    header.js 분리 후 적용

--- js/index.js
+++ js/index.js
@@ -15,22 +15,6 @@ var nightOffset;
 $(document).ready(function () {
     slideInitial();
 
-    //header에 사용되는 jq
-    $('.gnb li').mouseenter(function () {
-        $('.lnb, .bgcolor').stop().slideDown(200);
-    });
-    $('nav').mouseleave(function () {
-        $('.lnb, .bgcolor').stop().slideUp(200);
-    });
-    // searchbox 높이는 keywords의 높이값을 받아와서 늘어나도록
-    // keywords 누르면 input으로 값 전달하고 제출까지 되도록
-    $('.search').click(function () {
-        $('.search-box').stop().slideToggle(200);
-    });
-    // ---------------------------
-
-
-
     totalPage = $('.page').length;
     upcycleOffset = $('.upcycle').offset().top;
     dailyOffset = $('.daily').offset().top;
